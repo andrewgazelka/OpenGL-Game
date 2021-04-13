5b81e5d525d24fc4eee33520994697441e5e10ef
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-13 11:49:01 -0500

    add expect throw test case

--- test/test_file_reader.cpp
+++ test/test_file_reader.cpp
@@ -2,18 +2,25 @@
 #include "gtest/gtest.h"
 
 const auto FILE_NAME = "/Users/andrewgazelka/Projects/School/5607-cg/proj4/test/test.txt";
+const auto INVALID_FILE = "/Users/andrewgazelka/Projects/School/5607-cg/proj4/test/invalid.txt";
 
 namespace {
 
-    TEST(MapParser, WidthHeightRight) {
+    TEST(MapParser, MapGeneratedProperly) {
         Map map = MapParser::parseMap(FILE_NAME);
         EXPECT_EQ(map.height, 5);
         EXPECT_EQ(map.width, 5);
-        EXPECT_EQ(map.GetElement(0,0), Element::Empty());
-        EXPECT_EQ(map.GetElement(4,0), Element::Finish());
-        EXPECT_EQ(map.GetElement(1,1), Element::Wall());
-        EXPECT_EQ(map.GetElement(4,4), Element::Key(0));
-        EXPECT_EQ(map.GetElement(2,2), Element::Door(0));
+        EXPECT_EQ(map.GetElement(0, 0), Element::Empty());
+        EXPECT_EQ(map.GetElement(4, 0), Element::Finish());
+        EXPECT_EQ(map.GetElement(1, 1), Element::Wall());
+        EXPECT_EQ(map.GetElement(4, 4), Element::Key(0));
+        EXPECT_EQ(map.GetElement(2, 2), Element::Door(0));
     }
+
+    TEST(MapParser, InvalidDetected) {
+        EXPECT_THROW({ Map map = MapParser::parseMap(INVALID_FILE); },
+                     std::invalid_argument);
+
+    };
 }
 
